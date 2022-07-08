c87c7175e4233d8af79e3383c811586a4b0d8ca3
michael-siek <michaelsiek094@gmail.com>
2022-07-08 12:27:07 -0400

    feat(playwright): create a playwright example

---
+++ javascript/playwright/test/heroku.e2e.ts
@@ -0,0 +1,46 @@
+import 'mocha'
+import playwright from 'playwright'
+import { playwrightConfig } from '@deque/watcher'
+import { v4 } from 'uuid'
+
+describe('My Login Application', () => {
+  let page: playwright.Page
+  let browser: playwright.BrowserContext
+
+  const {
+    AXE_SERVER_URL = 'http://localhost:3000',
+    AXE_WATCHER_API_KEY = 'foobar'
+  } = process.env
+
+  if (!AXE_WATCHER_API_KEY) {
+    throw new Error('AXE_WATCHER_API_KEY is not defined')
+  }
+
+  const AXE_WATCHER_SESSION_ID = v4()
+  before(async () => {
+    browser = await playwright.chromium.launchPersistentContext(
+      /**
+       * pass an empty string so all of the browser session data, local storage saves to temp directory
+       * rather than project directory
+       *  */
+      '',
+      playwrightConfig({
+        axe: {
+          apiKey: AXE_WATCHER_API_KEY,
+          sessionId: AXE_WATCHER_SESSION_ID,
+          serverURL: AXE_SERVER_URL
+        }
+      })
+    )
+
+    page = await browser.newPage()
+  })
+
+  after(async () => {
+    await browser.close()
+  })
+
+  it('should login with valid credentials', async () => {
+    await page.goto('https://the-internet.herokuapp.com/login')
+  })
+})
