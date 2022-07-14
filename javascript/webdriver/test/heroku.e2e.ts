5ef411efbdf439e4e8e33f88bb18a155eb059cfd
Peter Schwendeman <peterschwende@gmail.com>
2022-07-14 09:34:31 -0400

    fix: update browserId to sessionId

--- javascript/webdriver/test/heroku.e2e.ts
+++ javascript/webdriver/test/heroku.e2e.ts
@@ -18,16 +18,17 @@ describe('My Login Application', () => {
   const AXE_WATCHER_SESSION_ID = v4()
   before(async () => {
     driver = await new Builder()
-    .forBrowser('chrome')
-    .setChromeOptions(
-      webdriverConfig({
-        axe: {
-          apiKey: AXE_WATCHER_API_KEY,
-          sessionId: AXE_WATCHER_SESSION_ID,
-          serverURL: AXE_SERVER_URL
-        }
-      })
-    ).build()
+      .forBrowser('chrome')
+      .setChromeOptions(
+        webdriverConfig({
+          axe: {
+            apiKey: AXE_WATCHER_API_KEY,
+            sessionId: AXE_WATCHER_SESSION_ID,
+            serverURL: AXE_SERVER_URL
+          }
+        })
+      )
+      .build()
   })
 
   after(async () => {
@@ -36,8 +37,10 @@ describe('My Login Application', () => {
 
   it('should login with valid credentials', async () => {
     await driver.get('https://the-internet.herokuapp.com/login')
-    await driver.findElement(By.css("#username")).sendKeys("tomsmith")
-    await driver.findElement(By.css("#password")).sendKeys("SuperSecretPassword!")
+    await driver.findElement(By.css('#username')).sendKeys('tomsmith')
+    await driver
+      .findElement(By.css('#password'))
+      .sendKeys('SuperSecretPassword!')
 
     await driver.findElement(By.css('button[type="submit"]')).click()
   })
