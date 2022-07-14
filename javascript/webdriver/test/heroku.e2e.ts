6335eb338db3e6a81a97e877a57f7b2364a16e4a
Peter Schwendeman <peterschwende@gmail.com>
2022-07-14 15:11:24 -0400

    feat(webdriver): add manual mode example

--- javascript/webdriver/test/heroku.e2e.ts
+++ javascript/webdriver/test/heroku.e2e.ts
@@ -1,10 +1,15 @@
 import 'mocha'
-import { Builder, WebDriver, By } from 'selenium-webdriver'
-import { webdriverConfig } from '@deque/watcher'
+import { Builder, By, WebDriver } from 'selenium-webdriver'
+import { Driver } from 'selenium-webdriver/chrome'
+import { webdriverConfig, WebdriverManualController } from '@deque/watcher'
 import { v4 } from 'uuid'
 
+function delay(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
 describe('My Login Application', () => {
-  let driver: WebDriver
+  let driver: any
 
   const {
     AXE_SERVER_URL = 'http://localhost:3000',
@@ -16,6 +21,7 @@ describe('My Login Application', () => {
   }
 
   const AXE_WATCHER_SESSION_ID = v4()
+
   before(async () => {
     driver = await new Builder()
       .forBrowser('chrome')
@@ -32,16 +38,43 @@ describe('My Login Application', () => {
   })
 
   after(async () => {
-    // await driver.quit()
+    await driver.quit()
   })
 
   it('should login with valid credentials', async () => {
     await driver.get('https://the-internet.herokuapp.com/login')
+    await delay(500)
+
     await driver.findElement(By.css('#username')).sendKeys('tomsmith')
     await driver
       .findElement(By.css('#password'))
       .sendKeys('SuperSecretPassword!')
-
     await driver.findElement(By.css('button[type="submit"]')).click()
+    await delay(500)
+  })
+
+  describe('Manual Mode', function () {
+    it('should login with valid credentials', async () => {
+      await driver.get('https://the-internet.herokuapp.com/login')
+      const axeController = new WebdriverManualController(driver)
+      await delay(500)
+
+      // Stop automatic axe analysis
+      await axeController.stop()
+      await axeController.analyze()
+
+      await driver.findElement(By.css('#username')).sendKeys('tomsmith')
+      await driver
+        .findElement(By.css('#password'))
+        .sendKeys('SuperSecretPassword!')
+      await driver.findElement(By.css('button[type="submit"]')).click()
+      await delay(500)
+
+      await axeController.stop()
+      await axeController.analyze()
+
+      // Restart automatic axe analysis
+      await axeController.stop()
+    })
   })
 })
