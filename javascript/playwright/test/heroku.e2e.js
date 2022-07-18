1a2e3e4a0a6052e5ede0619183298dc88b0294ba
Peter Schwendeman <peterschwende@gmail.com>
2022-07-18 11:43:10 -0400

    chore: change to js * remove lock file * commit requested changes

--- javascript/playwright/test/heroku.e2e.js
+++ javascript/playwright/test/heroku.e2e.js
@@ -1,29 +1,26 @@
-import 'mocha'
-import playwright from 'playwright'
-import { expect } from '@playwright/test'
-import { playwrightConfig, PlaywrightManualController } from '@deque/watcher'
-import { v4 } from 'uuid'
+const { chromium } = require('playwright')
+const { expect } = require('@playwright/test')
 
-function delay(ms: number) {
-  return new Promise(resolve => setTimeout(resolve, ms))
+const {
+  playwrightConfig,
+  PlaywrightManualController
+} = require('@deque/watcher')
+
+const {
+  AXE_SERVER_URL = 'https://axe.deque.com',
+  AXE_WATCHER_API_KEY = 'foobar'
+} = process.env
+
+if (!AXE_WATCHER_API_KEY) {
+  throw new Error('AXE_WATCHER_API_KEY is not defined')
 }
 
 describe('My Login Application', () => {
-  let page: playwright.Page
-  let browser: playwright.BrowserContext
+  let page
+  let browser
 
-  const {
-    AXE_SERVER_URL = 'http://localhost:3000',
-    AXE_WATCHER_API_KEY = 'foobar'
-  } = process.env
-
-  if (!AXE_WATCHER_API_KEY) {
-    throw new Error('AXE_WATCHER_API_KEY is not defined')
-  }
-
-  const AXE_WATCHER_SESSION_ID = v4()
   before(async () => {
-    browser = await playwright.chromium.launchPersistentContext(
+    browser = await chromium.launchPersistentContext(
       /**
        * pass an empty string so all of the browser session data, local storage saves to temp directory
        * rather than project directory
@@ -32,7 +29,6 @@ describe('My Login Application', () => {
       playwrightConfig({
         axe: {
           apiKey: AXE_WATCHER_API_KEY,
-          sessionId: AXE_WATCHER_SESSION_ID,
           serverURL: AXE_SERVER_URL
         }
       })
@@ -48,12 +44,11 @@ describe('My Login Application', () => {
   describe('Automatic Analysis', function () {
     it('should login with valid credentials', async () => {
       await page.goto('https://the-internet.herokuapp.com/login')
-      await delay(500)
 
       await page.locator('#username').type('tomsmith')
       await page.locator('#password').type('SuperSecretPassword!')
       await page.locator('button[type="submit"]').click()
-      await delay(500)
+      await page.locator('#flash').waitFor()
 
       // Assert 'You logged into a secure area!' exists and contains correct text
       await expect(page.locator('#flash')).toBeTruthy()
@@ -71,18 +66,16 @@ describe('My Login Application', () => {
       // Stop automatic axe analysis
       await axeController.stop()
       await axeController.analyze()
-      await delay(500)
 
       await page.locator('#username').type('tomsmith')
       await page.locator('#password').type('SuperSecretPassword!')
       await page.locator('button[type="submit"]').click()
-      await delay(500)
+      await page.locator('#flash').waitFor()
 
-      await axeController.stop()
       await axeController.analyze()
 
       // Restart automatic axe analysis
-      await axeController.stop()
+      await axeController.start()
 
       // Assert 'You logged into a secure area!' exists and contains correct text
       await expect(page.locator('#flash')).toBeTruthy()
