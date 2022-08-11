ee9eb1d08e0d68bd38d4fe4a9c72b9d94c0bf0e4
Peter Schwendeman <peterschwende@gmail.com>
2022-08-11 16:47:28 -0400

    feat: add generic wdio example

---
+++ javascript/wdio/test/heroku.e2e.js
@@ -0,0 +1,60 @@
+const { remote } = require('webdriverio')
+const { expect } = require('chai')
+
+const { wdioConfig, WdioManualController } = require('@deque/watcher')
+
+const { AXE_SERVER_URL, AXE_WATCHER_API_KEY } = process.env
+
+if (!AXE_WATCHER_API_KEY) {
+  throw new Error('AXE_WATCHER_API_KEY is not defined')
+}
+
+describe('My Login Application', () => {
+  let browser
+
+  before(async () => {
+    browser = await remote(wdioConfig({ axe: { apiKey: 'asdf' } }))
+  })
+
+  after(async () => {
+    await browser.deleteSession()
+  })
+
+  describe('Automatic Analysis', function () {
+    it('should login with valid credentials', async () => {
+      await browser.url('https://the-internet.herokuapp.com/login')
+
+      await browser.$('#username').setValue('tomsmith')
+      await browser.$('#password').setValue('SuperSecretPassword!')
+      await browser.$('button[type="submit"]').click()
+      await browser.$('#flash').waitForExist()
+
+      // Assert that 'You logged into a secure area!' element exists
+      expect(browser.$('#flash')).to.be.exist
+    })
+  })
+
+  describe('Manual Mode', function () {
+    it('should login with valid credentials', async () => {
+      await browser.url('https://the-internet.herokuapp.com/login')
+      const axeController = new WdioManualController(browser)
+
+      // Stop automatic axe analysis
+      await axeController.stop()
+      await axeController.analyze()
+
+      await browser.$('#username').setValue('tomsmith')
+      await browser.$('#password').setValue('SuperSecretPassword!')
+      await browser.$('button[type="submit"]').click()
+      await browser.$('#flash').waitForExist()
+
+      await axeController.analyze()
+
+      // Restart automatic axe analysis
+      await axeController.start()
+
+      // Assert that 'You logged into a secure area!' element exists
+      expect(browser.$('#flash')).to.be.exist
+    })
+  })
+})
