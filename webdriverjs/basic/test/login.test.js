bd385b783d37e494a2a86a7c63ca6fd60d4285f9
Stephen Mathieson <me@stephenmathieson.com>
2022-10-20 12:01:13 -0400

    ugly commit

--- webdriverjs/basic/test/login.test.js
+++ webdriverjs/basic/test/login.test.js
@@ -5,7 +5,17 @@ const { webdriverConfig, WebdriverController } = require('@axe-core/watcher')
 const { API_KEY, SERVER_URL } = process.env
 
 describe('My Login Application', () => {
-  let browser
+  let browser;
+  ;
+  ;
+  ;
+  ;
+  ;
+  ;
+  ;
+  ;
+  ;
+  ;
 
   before(async () => {
     browser = await new Builder()
