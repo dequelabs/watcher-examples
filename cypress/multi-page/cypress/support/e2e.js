7ff06aa9af672903b1ee3d6cc2d232d00fbf4b6d
Stephen Mathieson <me@stephenmathieson.com>
2023-01-04 17:51:29 -0500

    feat: Use "wrapped" Cypress driver

--- cypress/multi-page/cypress/support/e2e.js
+++ cypress/multi-page/cypress/support/e2e.js
@@ -1,6 +1,12 @@
 // Import the axe-watcher commands.
 require('@axe-core/watcher/dist/cypressCommands')
 
+// Wrap the Cypress driver.
+beforeEach(() => {
+  const { wrapCy } = require('@axe-core/watcher/dist/cypress')
+  wrapCy(cy)
+})
+
 // Flush axe-watcher results after each test.
 afterEach(() => {
   cy.axeFlush()
