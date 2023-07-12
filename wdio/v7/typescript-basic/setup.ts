e44b69d4871161455d189bfdce03a3cdc2b700bc
Gabe <41127686+Zidious@users.noreply.github.com>
2023-07-12 19:16:03 +0100

    feat: add WDIO v7 basic example

---
+++ wdio/v7/typescript-basic/setup.ts
@@ -0,0 +1,50 @@
+import 'mocha'
+import { wdioConfig, WdioController, wrapWdio } from '@axe-core/watcher'
+import { remote, type Browser } from 'webdriverio'
+
+/* Get your configuration from environment variables. */
+const { API_KEY, SERVER_URL = 'https://axe.deque.com' } = process.env
+
+/**
+ * As of V8, WebdriverIO removed the deprecated the @wdio/sync package. To use this on V7 of WebdriverIO
+ * you'll need to use the type `Browser<'async'>` instead of `Browser`.
+ *
+ * @see https://webdriver.io/blog/2021/07/28/sync-api-deprecation/
+ *
+ * In addition, if you're using Node 18 and encounter the following error:
+ * `FetchError: Failed to fetch browser webSocket URL from ...`.
+ *
+ * You may need to downgrade to Node 16.
+ *
+ * @see https://github.com/webdriverio/webdriverio/issues/8279
+ */
+let browser: Browser<'async'>
+
+let controller: WdioController
+
+before(async () => {
+  browser = await remote(
+    wdioConfig({
+      axe: {
+        apiKey: API_KEY as string,
+        serverURL: SERVER_URL
+      },
+      capabilities: {
+        browserName: 'chrome'
+      }
+    })
+  )
+
+  controller = new WdioController(browser)
+  wrapWdio(browser, controller)
+})
+
+afterEach(async () => {
+  await controller.flush()
+})
+
+after(async () => {
+  browser.deleteSession()
+})
+
+export { browser, controller }
