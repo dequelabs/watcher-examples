4286636baeac2ce75603674b317e735ddb6057a1
Dan Bjorge <dan.bjorge@deque.com>
2023-10-19 15:55:12 -0400

    format

--- webdriverjs/testing/tests/login.js
+++ webdriverjs/testing/tests/login.js
@@ -44,16 +44,18 @@ suite(env => {
     describe('with valid credentials', () => {
       it('should login', async () => {
         await browser.get('https://the-internet.herokuapp.com/login')
-  
+
         const username = await browser.findElement({ id: 'username' })
         const password = await browser.findElement({ id: 'password' })
-  
+
         await username.sendKeys('tomsmith')
         await password.sendKeys('SuperSecretPassword!')
-  
-        const submit = await browser.findElement({ css: 'button[type="submit"]' })
+
+        const submit = await browser.findElement({
+          css: 'button[type="submit"]'
+        })
         await submit.click()
-  
+
         await browser.wait(until.urlContains('/secure'))
       })
     })
