8345abd624954053056d3934ef4315bd842e0880
nayanrajDQ <nayan.adhikary@deque.com>
2025-01-13 16:50:08 +0530

    chore: added additional manual mode for wdio, puppeteer

---
+++ puppeteer/manual-mode/tests/login.js
@@ -0,0 +1,92 @@
+const { assert } = require('chai')
+const puppeteer = require('puppeteer')
+const {
+  wrapPuppeteerPage,
+  PuppeteerController,
+  puppeteerConfig
+} = require('@axe-core/watcher')
+
+/* Get your configuration from environment variables. */
+const { API_KEY, SERVER_URL = 'https://axe.deque.com' } = process.env
+
+describe('My Login Application', () => {
+  let browser
+  let page
+  let controller
+
+  before(async () => {
+    browser = await puppeteer.launch(
+      puppeteerConfig({
+        axe: {
+          apiKey: API_KEY,
+          serverURL: SERVER_URL,
+          /* Disable automatic analysis */
+          autoAnalyze: false
+        },
+        headless: false,
+        args: ['--headless=new']
+      })
+    )
+    // Create a page instance, using your browser instance.
+    page = await browser.newPage()
+
+    // Initialize the PuppeteerController by passing in the Puppeteer page.
+    controller = new PuppeteerController(page)
+
+    // Use the new wrapped Puppeteer page instance.
+    page = wrapPuppeteerPage(page, controller)
+  })
+
+  after(async () => {
+    await browser.close()
+  })
+
+  afterEach(async () => {
+    /* Flush axe-watcher results after each test. */
+    await controller.flush()
+    await page.close()
+  })
+
+  /*
+    Lets see the number of page state calculation
+
+    As the auto-analyze is false, it will not analyze automatically
+    Then we navigate to the page.
+    Then we analyze the page. (+1)
+    Then we fill the form.
+    Then Turn on auto-analyze.
+    Then we click the submit button (+1)
+    Then we wait for the element to appear.
+    Then we analyze the page. (+1)
+
+    So, the total number of page state calculation should be 3.
+  */
+
+  describe('Login', () => {
+    describe('with valid credentials', () => {
+      it('should login', async () => {
+        await page.goto('https://the-internet.herokuapp.com/login')
+
+        /* Analyze after navigation to the page */
+        await controller.analyze()
+
+        await page.type('#username', 'tomsmith')
+        await page.type('#password', 'SuperSecretPassword!')
+
+        /* starts auto-analyze to true */
+        await controller.start()
+
+        await page.click('button[type="submit"]')
+        const element = await page.waitForSelector('#flash')
+
+        /* stops auto-analyze */
+        await controller.stop()
+
+        /* Analyze after form submission */
+        await controller.analyze()
+
+        assert.isNotNull(element)
+      })
+    })
+  })
+})
