1c5f94cbd2e93d944001c9d14535aa8ce54a8247
DQ_Padmavathi <dq_padmavathi@MacBookPro.Dlink>
2025-07-11 19:44:12 +0530

    updated analyze cal type

--- cypress-component/manual-mode/cypress/tests/components/Button.cy.tsx
+++ cypress-component/manual-mode/cypress/tests/components/Button.cy.tsx
@@ -6,13 +6,13 @@ import { mount } from '@cypress/react';
 describe('Button Component', () => {
     // Test: Button renders correctly with default props and passes accessibility analysis
     it('renders correctly with default props', () => {
-         cy.axeWatcherStart();
+        cy.axeWatcherStart();
         mount(<Button label="Click me" />); // Mount the Button component
-         cy.axeWatcherAnalyse();
+        cy.axeWatcherAnalyze();
         // Assert the button text and existence
         cy.get('[data-testid="button-component"]').should('have.text', 'Click me');
         cy.get('[data-testid="button-component"]').should('exist');
-         cy.axeWatcherAnalyse();
+        cy.axeWatcherAnalyze();
         // Check button styles (instead of classes)
         cy.get('[data-testid="button-component"]').should('have.css', 'background-color', 'rgb(229, 231, 235)'); // Equivalent to bg-gray-200
         cy.get('[data-testid="button-component"]').should('have.css', 'color', 'rgb(156, 163, 175)'); // Equivalent to text-gray-400
