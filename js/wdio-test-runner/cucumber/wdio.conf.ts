a86d34aca38353f9cff9b018a5e77dbf6690f9ff
scottmries <scottmries@users.noreply.github.com>
2025-09-17 19:03:44 +0000

    :robot: Automated formatting fixes

--- js/wdio-test-runner/cucumber/wdio.conf.ts
+++ js/wdio-test-runner/cucumber/wdio.conf.ts
@@ -21,16 +21,16 @@ export const config = wdioTestRunner({
   capabilities: [
     {
       browserName: 'chrome',
-      'goog:chromeOptions': { 
+      'goog:chromeOptions': {
         args: ['--headless=new'],
         /*
-           * You can use the utility to get the Chrome binary path, including installing Chrome, if needed.
-           * This can be overridden by setting CHROME_BIN in the environment variables.
-           * If you do not specify a binary, the default Chrome installation will be used.
-           * This may cause issues, as Watcher does not support branded Chrome >= 139.
-           */
-          binary: getChromeBinaryPath()
-       }
+         * You can use the utility to get the Chrome binary path, including installing Chrome, if needed.
+         * This can be overridden by setting CHROME_BIN in the environment variables.
+         * If you do not specify a binary, the default Chrome installation will be used.
+         * This may cause issues, as Watcher does not support branded Chrome >= 139.
+         */
+        binary: getChromeBinaryPath()
+      }
     }
   ],
   logLevel: 'debug',
