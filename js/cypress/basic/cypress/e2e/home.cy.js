161cc08d012cd4a47a41b983e372008c8d562eaa
Scott Ries <scottmries@gmail.com>
2025-12-15 14:21:44 -0500

    feat: update test URL

---
+++ js/cypress/basic/cypress/e2e/home.cy.js
@@ -0,0 +1,9 @@
+describe('My Login Application', () => {
+  cy.visit('https://the-internet.herokuapp.com')
+      .get('ul li a[href="/login"]')
+      .should('exist')
+      .click()
+      .wait(1000)
+      .get('h2')
+      .should('have.text', 'Login Page')
+})
